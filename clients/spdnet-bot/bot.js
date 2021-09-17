19d96a823b49e9b532e6433f752cc394ec7c9c9c
saqfish <saqfish@gmail.com>
2021-09-16 22:22:53 -0500

    messages split by channel

--- clients/spdnet-bot/bot.js
+++ clients/spdnet-bot/bot.js
@@ -3,13 +3,11 @@ const { discord, server } = require("./config.json");
 const io = require("socket.io-client");
 
 // Util
-const send = (text) => {
-  if (channel && ready) channel.send(text);
+const sendTo = (o, text) => {
+  if (ready && o) o.send(text);
 };
 
-const sendTo = (o, text) => {
-  if (ready) o.send(text);
-};
+const sendToMain = (text) => sendTo(mainChannel, text);
 
 // Discord client
 const client = new Client({
@@ -23,10 +21,17 @@ const client = new Client({
 });
 
 // Globals
-let channel = null;
 let ready = false;
-let socket;
+let socket = null;
+
+let mainChannel = null;
+let spamChannel = null;
+let currentChannel = null;
+
+//TODO: set botname from discord info
 const botName = "spdnet-bot";
+
+//TODO: don't cache your own users, get them from server
 const players = new Map();
 
 // Begin bot
@@ -35,10 +40,20 @@ client.login(discord.key).catch((err) => {
 });
 
 client.on("ready", () => {
-  channel = client.channels.fetch(discord.channelId).then((c) => {
-    channel = c;
-    ready = true;
-  });
+  client.channels
+    .fetch(discord.mainChannelId)
+    .then((c) => {
+      mainChannel = c;
+    })
+    .catch((e) => console.log("get main channel", discod.mainChannelId, e));
+
+  client.channels
+    .fetch(discord.spamChannelId)
+    .then((c) => {
+      spamChannel = c;
+      ready = true;
+    })
+    .catch((e) => console.log("get spam channel", discod.spamChannelId, e));
 
   socket = io(server.address, {
     query: { version: 9999 },
@@ -47,9 +62,9 @@ client.on("ready", () => {
 
   const handler = socketHandler();
 
-  socket.on("chat", (id, nick, message) =>
-    handler.handleChat(id, nick, message)
-  );
+  socket.on("connect", () => sendToMain("Connected to SPDNet"));
+  socket.on("disconnected", () => sendToMain("Disconnected from SPDNet"));
+  socket.on("chat", (id, nick, message) => handler.handleChat(id, nick, message));
   socket.on("join", (nick, id) => handler.handleJoin(nick, id));
   socket.on("leave", (nick, id) => handler.handleLeave(nick, id));
   socket.on("action", (type, data) => handler.handleAction(type, data));
@@ -60,21 +75,22 @@ client.on("ready", () => {
 const socketHandler = () => {
   return {
     handleConnectionError: (err) => console.log(err),
-    handleChat: (id, nick, message) => send(`**${nick}**: ${message}`),
+    handleChat: (id, nick, message) =>
+      sendTo(spamChannel, `**${nick}**: ${message}`),
     handleJoin: (nick, id) => {
       players.set(nick, id);
-      send(`*${nick} has joined*`);
+      sendTo(spamChannel, `*${nick} has joined*`);
     },
     handleLeave: (nick) => {
       players.delete(nick);
-      send(`*${nick} has left*`);
+      sendTo(spamChannel, `*${nick} has left*`);
     },
     handleAction: (type, data) => {
       const actions = {
         GLOG: 5,
       };
       const process = {
-        [actions.GLOG]: (data) => send(`*${data.msg}*`),
+        [actions.GLOG]: (data) => sendTo(spamChannel, `*${data.msg}*`),
       };
       process[type](JSON.parse(data));
     },
@@ -84,19 +100,20 @@ const socketHandler = () => {
 // Command handler
 const cmd = (text, user) => {
   handle = {
-    taco: () => send(`taco?!`),
+    taco: () => sendTo(currentChannel, `taco?!`),
     say: ({ args }) => {
-      if (args.length) send(args.join(" "));
+      if (args.length) sendTo(currentChannel, args.join(" "));
     },
     online: () => {
       if (players.size) {
         const list = Array.from(players.keys()).join(", ");
-        send(list);
-      } else send("No one is playing!");
+        sendTo(currentChannel, list);
+      } else sendTo(currentChannel, "No one is playing!");
     },
     give: ({ args }) => {
-      if (args.length < 5) {
-        send(
+      if (args.length < 4) {
+        sendTo(
+          currentChannel,
           `\`!give [item count] [item class name ] [item level] [player name]\`\n 
 	     Player names are case sensitive. Use !online to see a list of current players`
         );
@@ -111,12 +128,12 @@ const cmd = (text, user) => {
       const isValidNumber = count > 0 && count < 4;
 
       if (!count) {
-        send(`Invalid item count!`);
+        sendTo(currentChannel, `Invalid item count!`);
         return;
       }
 
       if (!level) {
-        send(`Invalid item level!`);
+        sendTo(currentChannel, `Invalid item level!`);
         return;
       }
 
@@ -136,10 +153,11 @@ const cmd = (text, user) => {
               () => {}
             );
         } else
-          send(
+          sendTo(
+            currentChannel,
             `Invalid number: ${numberofItems}. Number of items must be 1 - 3.`
           );
-      } else send(`No player: ${playerName}!`);
+      } else sendTo(currentChannel, `No player: ${playerName}!`);
     },
     register: ({ user }) => {
       const { id, username, discriminator } = user;
@@ -147,6 +165,15 @@ const cmd = (text, user) => {
         sendTo(user, `Your key is ${key}`)
       );
     },
+    help: () =>
+      sendTo(
+        currentChannel,
+        "`!register` - register for SPDNet\n" +
+          "`!online` - list online players\n" +
+          "`!give [item count] [item class name ] [item level] [player name]` - give a player an item\n" +
+          "`!say [text]` - say text\n" +
+          "`!help` - this help"
+      ),
     default: () => {
       // Unlisted command action here
     },
@@ -160,7 +187,8 @@ const cmd = (text, user) => {
 
 // Bot events
 client.on("messageCreate", (message) => {
-  const { content, author } = message;
+  const { content, author, channel } = message;
   const username = author.username;
+  currentChannel = channel;
   if (!username.includes(botName)) cmd(content, author);
 });
